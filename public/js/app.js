a864702560aaa12355b5d061bc18e4fd493fce82
milac0 <milac0@hotmail.com>
2019-08-06 13:23:15 +0200

    changed ports

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e) => {
     msgTwo.textContent = ''
     const location = search.value
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 msgOne.textContent = data.error
